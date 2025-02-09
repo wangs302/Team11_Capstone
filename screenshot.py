534b75a9118fb866f0e4f9cf4c31d7d15ff87825
Bianca Ha <biancaha2001@gmail.com>
2025-02-09 17:28:09 -0500

    written into function, takes screenshot at every frame between defined period of time

--- screenshot.py
+++ screenshot.py
@@ -1,13 +1,36 @@
 import cv2 
-
-cap = cv2.VideoCapture("fireworks.mp4")
-
-if cap.isOpened():
-    cap.set(cv2.CAP_PROP_POS_FRAMES, 100) # like setting cursor to that frame
-    ret, frame = cap.read() #grabs video frame
-
-    if ret:
-        cv2.imwrite("test.jpg",frame) # save image
+import os
 
 
-cap.release()
+def save_images(video_path,t_start,t_stop,photo_path):
+    cap = cv2.VideoCapture(video_path)
+
+    if cap.isOpened():
+        # check if photo directory exists, and make it if it doesn't
+        try:
+            os.makedirs(photo_path)
+        except FileExistsError:
+            # directory already exists
+            pass
+
+        fps = cap.get(cv2.CAP_PROP_FPS) # get frame_rate
+
+        f_start = round(t_start*fps)
+        f_stop = round(t_stop*fps)
+
+        for f in range(f_start,f_stop,1): # for every frame in that time period
+            cap.set(cv2.CAP_PROP_POS_FRAMES, f) # like setting cursor to that frame
+            ret, frame = cap.read() #grabs video frame
+            
+            if ret:
+                pic = photo_path + "frame_{}.jpg".format(f) #photo name
+                cv2.imwrite(pic,frame) # save image
+
+    cap.release()
+
+
+save_images("patient1/jan27.mp4",0.5,0.6,"patient1/photos/")
+
+
+
+
