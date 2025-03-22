36deb4feb1ed15140ebf9db9fae2cf1e5cba1d76
wangs302 <wangs302@mcmaster.ca>
2025-03-22 13:41:08 -0400

    save files

--- RoxxTesting/flutter_application_1/lib/main.dart
+++ RoxxTesting/flutter_application_1/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
 import 'package:camera/camera.dart';
+import 'package:gal/gal.dart';
 
 void main() {
   runApp(MyApp());
@@ -536,259 +537,6 @@ class SetupPage extends StatelessWidget {
   }
 }
 
-
-
-
-class CameraPage extends StatefulWidget {
-  const CameraPage({super.key});
-
-  @override
-  CameraPageState createState() => CameraPageState();
-}
-
-class CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
-  CameraController? _controller;
-  bool _isCameraInitialized = false;
-  late final List<CameraDescription> _cameras;
-  bool _isRecording = false;
-  bool _isFlashOn = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    initCamera();
-  }
-
-  Future<void> initCamera() async {
-    _cameras = await availableCameras();
-    // Initialize the camera with the first camera in the list
-    await onNewCameraSelected(_cameras.first);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    // App state changed before we got the chance to initialize.
-    final CameraController? cameraController = _controller;
-
-    // App state changed before we got the chance to initialize.
-    if (cameraController == null || !cameraController.value.isInitialized) {
-      return;
-    }
-
-    if (state == AppLifecycleState.inactive) {
-      // Free up memory when camera not active
-      cameraController.dispose();
-    } else if (state == AppLifecycleState.resumed) {
-      // Reinitialize the camera with same properties
-      onNewCameraSelected(cameraController.description);
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  Future<XFile?> captureVideo() async {
-    final CameraController? cameraController = _controller;
-    try {
-      setState(() {
-        _isRecording = true;
-      });
-      await cameraController?.startVideoRecording();
-      await Future.delayed(const Duration(seconds: 5));
-      final video = await cameraController?.stopVideoRecording();
-      setState(() {
-        _isRecording = false;
-      });
-      return video;
-    } on CameraException catch (e) {
-      debugPrint('Error: $e');
-      return null;
-    }
-  }
-
-  void _onRecordVideoPressed() async {
-    final navigator = Navigator.of(context);
-    final xFile = await captureVideo();
-    if (xFile != null) {
-      if (xFile.path.isNotEmpty) {
-        navigator.push(
-          MaterialPageRoute(
-            builder: (context) => PreviewPage(
-              videoPath: xFile.path,
-            ),
-          ),
-        );
-      }
-    }
-  }
-
-  void _Flashlight() async{
-    if (_controller != null) {
-      setState(() {
-        _isFlashOn = !_isFlashOn;
-      });
-      await _controller!.setFlashMode(
-        _isFlashOn ? FlashMode.torch : FlashMode.off,
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isCameraInitialized) {
-      return SafeArea(
-        child: Scaffold(
-          body: Column(
-            children: [
-              CameraPreview(_controller!),
-              const SizedBox(
-                height: 35,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-
-                  if (!_isRecording) const SizedBox(width: 15),
-                  ElevatedButton(
-                    onPressed:_isRecording? null: _onRecordVideoPressed,
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(70, 70),
-                        shape: const CircleBorder(),
-                        backgroundColor: Colors.white),
-                    child: Icon(
-                      _isRecording ? Icons.stop : Icons.videocam,
-                      color: Colors.red,
-                    ),
-                  ),
-                  SizedBox(width:30),
-                  ElevatedButton(
-                      onPressed: _Flashlight,
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(40,40),
-                        backgroundColor: Colors.black
-                      ),
-                      child: Icon(
-                        _isFlashOn ? Icons.flash_on : Icons.flash_off,
-                        color: _isFlashOn ? Colors.yellow : Colors.white,
-                      )
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-    } else {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-  }
-
-  Future<void> onNewCameraSelected(CameraDescription description) async {
-    final previousCameraController = _controller;
-
-    // Instantiating the camera controller
-    final CameraController cameraController = CameraController(
-      description,
-      ResolutionPreset.high,
-      imageFormatGroup: ImageFormatGroup.jpeg,
-    );
-
-    // Initialize controller
-    try {
-      await cameraController.initialize();
-    } on CameraException catch (e) {
-      debugPrint('Error initializing camera: $e');
-    }
-    // Dispose the previous controller
-    await previousCameraController?.dispose();
-
-    // Replace with the new controller
-    if (mounted) {
-      setState(() {
-        _controller = cameraController;
-      });
-    }
-
-    // Update UI if controller updated
-    cameraController.addListener(() {
-      if (mounted) setState(() {});
-    });
-
-    // Update the Boolean
-    if (mounted) {
-      setState(() {
-        _isCameraInitialized = _controller!.value.isInitialized;
-      });
-    }
-  }
-}
-
-class PreviewPage extends StatefulWidget {
-  final String? imagePath;
-  final String? videoPath;
-
-  const PreviewPage({Key? key, this.imagePath, this.videoPath})
-      : super(key: key);
-
-  @override
-  State<PreviewPage> createState() => _PreviewPageState();
-}
-
-class _PreviewPageState extends State<PreviewPage> {
-  VideoPlayerController? controller;
-
-  Future<void> _startVideoPlayer() async {
-    if (widget.videoPath != null) {
-      controller = VideoPlayerController.file(File(widget.videoPath!));
-      await controller!.initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized,
-        // even before the play button has been pressed.
-        setState(() {});
-      });
-      await controller!.setLooping(true);
-      await controller!.play();
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.videoPath != null) {
-      _startVideoPlayer();
-    }
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: widget.imagePath != null
-            ? Image.file(
-          File(widget.imagePath ?? ""),
-          fit: BoxFit.cover,
-        )
-            : AspectRatio(
-          aspectRatio: controller!.value.aspectRatio,
-          child: VideoPlayer(controller!),
-        ),
-      ),
-    );
-  }
-}
-
 class TrainingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -949,3 +697,287 @@ class PatientPage extends StatelessWidget {
     );
   }
 }
+
+
+
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  CameraPageState createState() => CameraPageState();
+}
+
+class CameraPageState extends State<CameraPage> with WidgetsBindingObserver {
+  CameraController? _controller;
+  bool _isCameraInitialized = false;
+  late final List<CameraDescription> _cameras;
+  bool _isRecording = false;
+  bool _isFlashOn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    initCamera();
+  }
+
+  Future<void> initCamera() async {
+    _cameras = await availableCameras();
+    // Initialize the camera with the first camera in the list
+    await onNewCameraSelected(_cameras.first);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // App state changed before we got the chance to initialize.
+    final CameraController? cameraController = _controller;
+
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      // Free up memory when camera not active
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      // Reinitialize the camera with same properties
+      onNewCameraSelected(cameraController.description);
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  Future<XFile?> captureVideo() async {
+    final CameraController? cameraController = _controller;
+    try {
+      setState(() {
+        _isRecording = true;
+      });
+      await cameraController?.startVideoRecording();
+      await Future.delayed(const Duration(seconds: 5));
+      final video = await cameraController?.stopVideoRecording();
+      setState(() {
+        _isRecording = false;
+      });
+      return video;
+    } on CameraException catch (e) {
+      debugPrint('Error: $e');
+      return null;
+    }
+  }
+
+  void _onRecordVideoPressed() async {
+    final navigator = Navigator.of(context);
+    final xFile = await captureVideo();
+    if (xFile != null) {
+      Gal.putVideo(xFile.path);
+      if (xFile.path.isNotEmpty) {
+        navigator.push(
+          MaterialPageRoute(
+            builder: (context) => PreviewPage(
+              videoPath: xFile.path,
+            ),
+          ),
+        );
+      }
+    }
+  }
+
+  void _Flashlight() async{
+    if (_controller != null) {
+      setState(() {
+        _isFlashOn = !_isFlashOn;
+      });
+      await _controller!.setFlashMode(
+        _isFlashOn ? FlashMode.torch : FlashMode.off,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isCameraInitialized) {
+      return SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              CameraPreview(_controller!),
+              const SizedBox(
+                height: 35,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  if (!_isRecording) const SizedBox(width: 15),
+                  ElevatedButton(
+                    onPressed:_isRecording? null: _onRecordVideoPressed,
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(70, 70),
+                        shape: const CircleBorder(),
+                        backgroundColor: Colors.white),
+                    child: Icon(
+                      _isRecording ? Icons.stop : Icons.videocam,
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox(width:30),
+                  ElevatedButton(
+                      onPressed: _Flashlight,
+                      style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(40,40),
+                          backgroundColor: Colors.black
+                      ),
+                      child: Icon(
+                        _isFlashOn ? Icons.flash_on : Icons.flash_off,
+                        color: _isFlashOn ? Colors.yellow : Colors.white,
+                      )
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+
+  Future<void> onNewCameraSelected(CameraDescription description) async {
+    final previousCameraController = _controller;
+
+    // Instantiating the camera controller
+    final CameraController cameraController = CameraController(
+      description,
+      ResolutionPreset.high,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+
+    );
+
+    // Initialize controller
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      debugPrint('Error initializing camera: $e');
+    }
+    // Dispose the previous controller
+    await previousCameraController?.dispose();
+
+    // Replace with the new controller
+    if (mounted) {
+      setState(() {
+        _controller = cameraController;
+      });
+    }
+
+    // Update UI if controller updated
+    cameraController.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    // Update the Boolean
+    if (mounted) {
+      setState(() {
+        _isCameraInitialized = _controller!.value.isInitialized;
+      });
+    }
+  }
+}
+
+class PreviewPage extends StatefulWidget {
+  final String? imagePath;
+  final String? videoPath;
+
+  const PreviewPage({Key? key, this.imagePath, this.videoPath})
+      : super(key: key);
+
+  @override
+  State<PreviewPage> createState() => _PreviewPageState();
+}
+
+class _PreviewPageState extends State<PreviewPage> {
+  VideoPlayerController? controller;
+
+  Future<void> _startVideoPlayer() async {
+    if (widget.videoPath != null) {
+      controller = VideoPlayerController.file(File(widget.videoPath!));
+      await controller!.initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized,
+        // even before the play button has been pressed.
+        setState(() {});
+      });
+      await controller!.setLooping(true);
+      await controller!.play();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.videoPath != null) {
+      _startVideoPlayer();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+            padding:  EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: widget.imagePath != null
+                      ? Image.file(
+                    File(widget.imagePath ?? ""),
+                    fit: BoxFit.cover,
+                  )
+                      : AspectRatio(
+                    aspectRatio: controller!.value.aspectRatio,
+                    child: VideoPlayer(controller!),
+                  ),
+                ),
+                SizedBox(height:100),
+                ElevatedButton(
+                  onPressed: () => Navigator.pushNamed(context,'/home'),
+
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                    child: Text('Finish Scan', style: TextStyle(fontSize: 20, color: Colors.white)),
+                  ),
+                ),
+
+              ],
+            )
+        )
+
+    );
+  }
+}
+
+
--- RoxxTesting/flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ RoxxTesting/flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import gal
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
