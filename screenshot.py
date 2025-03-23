f20c578566dbc7f8ff0e65f690d554f3d6ab9632
Bianca Ha <biancaha2001@gmail.com>
2025-03-22 21:38:06 -0400

    edits to file path + saving naming conventions

--- screenshot.py
+++ screenshot.py
@@ -2,34 +2,37 @@ import cv2
 import os
 
 
-def save_images(video_path,photo_path,t_start,t_stop):
+def save_images(video_path,photo_path):
     cap = cv2.VideoCapture(video_path)
 
     if cap.isOpened():
         # check if photo directory exists, and make it if it doesn't
         try:
-            os.makedirs(photo_path)
+            os.makedirs(photo_path+'raw/')
         except FileExistsError:
             # directory already exists
             pass
-
-        fps = cap.get(cv2.CAP_PROP_FPS) # get frame_rate
+        
+        fps = int(cap.get(cv2.CAP_PROP_FPS)) # get frame_rate
+        n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        t_start = 0
+        t_stop = n_frames/fps # could also just use the number of frames so lines below are irrelavent but keeping modular if want to specify later by time
 
         f_start = round(t_start*fps)
         f_stop = round(t_stop*fps)
 
-        for f in range(f_start,f_stop,1): # for every frame in that time period
+        for f in range(f_start,f_stop,round(fps/2)): 
             cap.set(cv2.CAP_PROP_POS_FRAMES, f) # like setting cursor to that frame
             ret, frame = cap.read() #grabs video frame
             
             if ret:
-                pic = photo_path + "frame_{}.jpg".format(f) #photo name
+                pic = photo_path + "raw/frame_{}.jpg".format(f) #photo name
                 cv2.imwrite(pic,frame) # save image
 
     cap.release()
 
 
-save_images("patient1/20250304_160003.mp4","patient1/photos/",9.1,9.2)
+save_images("patient1/mar4_cropped.mp4","patient1/photos/")
 
 
 
